b4ccea94a4090920f4821546a1539552daafdb31
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2026-07-29 16:45:00 -0300

    Página do Feed feita pelo José

---
+++ Models/Post.cs
@@ -0,0 +1,23 @@
+﻿namespace FIsioNet.Models;
+
+public class Post
+{
+    // Autor
+    public string AuthorName { get; set; } = string.Empty;
+    public string AuthorSpecialty { get; set; } = string.Empty;
+    public string AuthorRegistration { get; set; } = string.Empty;
+    public bool AuthorIsVerified { get; set; }
+    public string AuthorAvatar { get; set; } = string.Empty;
+
+    // Conteúdo do post
+    public string MediaSource { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    // Tags (CID, método, etc.)
+    public string TagCid { get; set; } = string.Empty;
+    public string TagMethod { get; set; } = string.Empty;
+
+    // Interações
+    public string LikesCount { get; set; } = "0";
+    public string SavesCount { get; set; } = "0";
+}
